30dfa04c1c0842aaeeabe42b9ae94fc316353b40
arfrank <arfrank@gmail.com>
2011-07-30 09:13:20 -0400

    Redid settings; add a quick dashboard widget; and added css

--- js/callme.js
+++ js/callme.js
@@ -1,3 +1,22 @@
-$(document).ready(function() {
-	
+jQuery(document).ready(function($) {
+	Twilio.Device.setup(token);
+
+      Twilio.Device.ready(function (device) {
+        $("#log").text("Ready");
+      });
+
+      Twilio.Device.error(function (error) {
+        $("#log").text("Error: " + error.message);
+      });
+
+      Twilio.Device.connect(function (conn) {
+        $("#log").text("Successfully established call");
+      });
+
+      function call() {
+        Twilio.Device.connect();
+      }
+	$('#callme_widget').click(function(event) {
+			call();
+	});
 });
