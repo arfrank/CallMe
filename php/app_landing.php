88a48a7eb4867fc2f0d7cbd2dcae0535b9bbd99a
arfrank <arfrank@gmail.com>
2011-08-01 19:17:25 -0400

    OOPS bug fix

--- php/app_landing.php
+++ php/app_landing.php
@@ -9,7 +9,7 @@ if (!function_exists('add_action')) {
         require_once($wp_root.'/wp-config.php');
     }
 }
-include 'TwilioLibrary/Services/Twilio.php';
+require_once('TwilioLibrary/Services/Twilio.php');
 
 /*
 $token = 'YYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYY';
