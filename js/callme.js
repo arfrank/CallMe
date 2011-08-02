2bf494faef38f9e3174e734280656ae9353ee34e
arfrank <arfrank@gmail.com>
2011-08-01 23:57:20 -0400

    Styling the opening/connecting

--- js/callme.js
+++ js/callme.js
@@ -1,16 +1,18 @@
 jQuery(document).ready(function($) {
 	Twilio.Device.setup(token);
-
+	$callme_widget = $('#callme_widget');
       Twilio.Device.ready(function (device) {
-        $("#log").text("Ready");
+	    if ($callme_widget.hasClass('callme_bottomleft') || $callme_widget.hasClass('callme_bottomright') ) {
+			$callme_widget.animate({'bottom':'0px'});
+		}
+		if ($callme_widget.hasClass('callme_topleft') || $callme_widget.hasClass('callme_topright') ) {
+			$callme_widget.animate({'top':'0px'});	
+		}
+		
       });
 
       Twilio.Device.error(function (error) {
-        $("#log").text("Error: " + error.message);
-      });
-
-      Twilio.Device.connect(function (conn) {
-        $("#log").text("Successfully established call");
+		console.log('An error occurred loading the CallMe Widget - '+error);
       });
 
       function call() {
