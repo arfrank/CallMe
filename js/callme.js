139e72f2c663090debafaaecb65c1275b980a37e
arfrank <arfrank@gmail.com>
2011-08-02 23:10:13 -0600

    Getting conferencing working

--- js/callme.js
+++ js/callme.js
@@ -1,26 +1,28 @@
 jQuery(document).ready(function($) {
 	Twilio.Device.setup(token);
-	$callme_widget = $('#callme_widget');
-      Twilio.Device.ready(function (device) {
-	    if ($callme_widget.hasClass('callme_bottomleft') || $callme_widget.hasClass('callme_bottomright') ) {
-			$callme_widget.animate({'bottom':'0px'});
-		}
-		if ($callme_widget.hasClass('callme_topleft') || $callme_widget.hasClass('callme_topright') ) {
-			$callme_widget.animate({'top':'0px'});	
-		}
+	Twilio.Device.ready(function (device) {
+		var callme_widget = $('#callme_widget');
+		setTimeout(function(){
+			if (callme_widget.hasClass('callme_bottomleft') || callme_widget.hasClass('callme_bottomright') ) {
+				callme_widget.animate({'bottom':'0px'});
+			}
+			if (callme_widget.hasClass('callme_topleft') || callme_widget.hasClass('callme_topright') ) {
+				callme_widget.animate({'top':'0px'});	
+			}
+		}, 1000);
 		if (autoconnect_conference != undefined && autoconnect_conference) {
 			call();
 		};
-      });
+	});
 
-      Twilio.Device.error(function (error) {
+	Twilio.Device.error(function (error) {
 		console.log('An error occurred loading the CallMe Widget - '+error);
-      });
+	});
 
-      function call() {
-        Twilio.Device.connect();
-      }
+	function call() {
+		Twilio.Device.connect();
+	}
 	$('#callme_widget').click(function(event) {
-			call();
+		call();
 	});
 });
